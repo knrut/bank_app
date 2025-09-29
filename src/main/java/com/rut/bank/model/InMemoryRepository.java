52e2bad1b0685c4b60e3f7d8df654b9ffa67d692
krystiannowakowski <kn@airkryswakowski.home>
2025-09-29 15:30:44 +0200

    implement InMemoryRepository

---
+++ src/main/java/com/rut/bank/model/InMemoryRepository.java
@@ -0,0 +1,39 @@
+package com.rut.bank.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class InMemoryRepository<ID, E extends Entity<ID>> implements Repository<ID, E> {
+    private final List<E> db = new ArrayList<>();
+
+    @Override
+    public List<E> findALL() {
+        return db;
+    }
+
+    @Override
+    public Optional<E> findById(ID id) {
+        return db.stream()
+                .filter(e -> id.equals(e.getID()))
+                .findFirst();
+    }
+
+    @Override
+    public E save(E e) {
+        db.add(e);
+        return e;
+    }
+
+    @Override
+    public E update(E e) {
+        delete(e);
+        db.add(e);
+        return e;
+    }
+
+    @Override
+    public void delete(E e) {
+        db.remove(e);
+    }
+}
