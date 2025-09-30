c5c85c2e1ac8f51e99a4e0b9d95fb8c4dbc187d0
krystiannowakowski <kn@airkryswakowski.home>
2025-09-30 11:15:20 +0200

    feat: add BankAccountView

---
+++ src/main/java/com/rut/bank/model/BankAccountView.java
@@ -0,0 +1,27 @@
+package com.rut.bank.model;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class BankAccountView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String askForInput(String message) {
+        printMessage(message);
+        return scanner.nextLine();
+    }
+
+    public BigDecimal askForAmount(String message) {
+        String input = askForInput(message);
+        try {
+            return new BigDecimal(input);
+        }
+        catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
