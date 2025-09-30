94dc95130d40801c13a50d6fb7024e0668b27516
krystiannowakowski <kn@airkryswakowski.home>
2025-09-30 17:38:16 +0200

    feat: add LoginFormController

---
+++ src/main/java/com/rut/bank/controller/LoginFormController.java
@@ -0,0 +1,49 @@
+package com.rut.bank.controller;
+
+import com.rut.bank.model.BankAccountService;
+import com.rut.bank.model.Client;
+import com.rut.bank.repository.ClientRepository;
+import com.rut.bank.view.LoginForm;
+
+import javax.swing.*;
+
+public class LoginFormController {
+    private final BankAccountService service;
+    private final LoginForm loginForm;
+    
+    public LoginFormController(ClientRepository repository) {
+        this.service = new BankAccountService(repository);
+        this.loginForm = new LoginForm();
+        control();
+    }
+
+    private void control() {
+        loginForm.getButtonRegister().addActionListener(e -> onRegister());
+        loginForm.getButtonLogin().addActionListener(e -> onLogin());
+    }
+
+    private void onRegister() {
+        String login = loginForm.getTextFieldLogin().getText();
+        String password = new String(loginForm.getTextFieldPassword().getPassword());
+
+        if (service.registerUser(login, password)) {
+            JOptionPane.showMessageDialog(loginForm.getFrame(), "User registered: " + login);
+        }
+        else {
+            JOptionPane.showMessageDialog(loginForm.getFrame(), "Login already taken!");
+        }
+    }
+
+    private void onLogin() {
+        String login = loginForm.getTextFieldLogin().getText();
+        String password = new String(loginForm.getTextFieldPassword().getPassword());
+
+        if (service.loginUser(login, password)) {
+            JOptionPane.showMessageDialog(loginForm.getFrame(), "Logged in as: " + login);
+            // TODO: -> UserPanel / AdminPanel
+        }
+        else {
+            JOptionPane.showMessageDialog(loginForm.getFrame(), "Incorrect login or password");
+        }
+    }
+}
