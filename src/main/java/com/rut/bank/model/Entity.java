19581e27989210b3ef5256b5901a30c0f0b06d26
krystiannowakowski <kn@airkryswakowski.home>
2025-10-17 15:37:15 +0200

    feat: fix errors

--- src/main/java/com/rut/bank/model/Entity.java
+++ src/main/java/com/rut/bank/model/Entity.java
@@ -1,6 +1,10 @@
 package com.rut.bank.model;
 
-public abstract class Entity<ID> {
+import java.io.Serializable;
+
+public abstract class Entity<ID> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private ID id;
 
     public ID getID() {
