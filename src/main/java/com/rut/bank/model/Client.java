5360eb1fb77987f0ea2ce2fafd0311148139e54b
krystiannowakowski <kn@airkryswakowski.home>
2025-11-02 21:05:46 +0100

    add Client class

--- src/main/java/com/rut/bank/model/Client.java
+++ src/main/java/com/rut/bank/model/Client.java
@@ -3,14 +3,32 @@ package com.rut.bank.model;
 import java.util.UUID;
 
 public class Client extends Entity<UUID> {
-    String firstName;
-    String lastName;
-    String age;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private BankAccount assignedBankAccount;
 
-    public boolean getFirstName() {
+    public Client(String firstName, String lastName, int age, BankAccount assignedBankAccount) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.assignedBankAccount = assignedBankAccount;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public BankAccount getAssignedBankAccount() {
+        return assignedBankAccount;
     }
 }
 
